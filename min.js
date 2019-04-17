041c0f4df71b88071bb4759355fda0f9d7bf605d
RainbowEye0486 <47811511+RainbowEye0486@users.noreply.github.com>
2019-04-17 10:21:00 +0800

    1231
    2123

---
+++ min.js
@@ -0,0 +1,32 @@
+
+
+function picmove1() {
+    document.getElementById("name").innerHTML = "Cheng Ting-Wei";
+    document.getElementById("content").innerHTML = "Systems and Naval Mechatronic Engineering,<br>No one can touch my shoulder or I will let you know who's your daddy.";
+    document.getElementById("innershow").className="slideup";
+}
+  function picmove2() {
+    document.getElementById("name").innerHTML = "Chih-Chun Lin";
+    document.getElementById("content").innerHTML =" My major is industrial design. I have a lot of hobbies such as drawing, metalworking, sewing bags. <br>My short-term goal is keep things on track and explore my limits.I hope this cooperation will go smoothly.";
+    document.getElementById("innershow").className="slideup";
+  }
+  function picmove3() {
+    document.getElementById("name").innerHTML = "Chien Yu-Cheng";
+    document.getElementById("content").innerHTML = "Systems and Naval Mechatronic Engineering,<br>You can also call me Ben because I will make you Bang.";
+    document.getElementById("innershow").className="slideup";
+  }
+  function picmove4() {
+    document.getElementById("name").innerHTML = "Chao Chia-Wei";
+    document.getElementById("content").innerHTML="Hello everyone, my name is Chih-Chun Lin. My major is industrial design. I have a lot of hobbies such as drawing, metalworking, sewing bags. My short-term goal is keep things on track and explore my limits.I hope this cooperation will go smoothly.";
+    document.getElementById("innershow").className="slideup";
+  }
+  function picmove5() {
+    document.getElementById("name").innerHTML = "Zhang Chen-Han";
+    document.getElementById("content").innerHTML ="Electrical Engineering,<br>Another one bite the dust.";
+    document.getElementById("innershow").className="slideup";
+  }
+  function picgo() {
+    document.getElementById("name").innerHTML = "";
+    document.getElementById("content").innerHTML="";
+    document.getElementById("innershow").className="slide";
+  }
